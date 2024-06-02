158ade1cb41cdbd91266edbefc9782907f97776e
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-06-02 18:54:10 -0300

    Add donation crud

--- src/main/java/org/ijb/stock/helper/DonationHelper.java
+++ src/main/java/org/ijb/stock/helper/DonationHelper.java
@@ -1,2 +1,53 @@
-package org.ijb.stock.helper;public class DonationHelper {
+package org.ijb.stock.helper;
+
+import org.ijb.stock.model.Donation;
+import org.ijb.stock.model.dto.DonationDTO;
+import org.ijb.stock.service.DonorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DonationHelper {
+
+    @Autowired
+    private DonorService donorService;
+
+    public Donation dtoToDonation(DonationDTO donationDTO) {
+        return new Donation(donorService.getById(donationDTO.getIdDonor()), donationDTO.getDescription(), donationDTO.getQuantity());
+    }
+
+    public DonationDTO donationToDTO(Donation donation) {
+        return new DonationDTO(donation.getId(), donation.getId(), donation.getDescription(), donation.getQuantity());
+    }
+
+    public List<DonationDTO> donationsToDTO(List<Donation> donations) {
+        List<DonationDTO> donationDTOs = new ArrayList<>();
+        for (Donation donation : donations) {
+            donationDTOs.add(donationToDTO(donation));
+        }
+
+        return donationDTOs;
+    }
+
+    public Donation donationUpdateSetter(Donation donation, DonationDTO donationDTO) {
+        donation.setDonor(donorService.getById(donationDTO.getIdDonor()));
+        donation.setDescription(donationDTO.getDescription());
+        donation.setQuantity(donationDTO.getQuantity());
+
+        return donation;
+    }
+
+    public Donation donationPatchUpdateSetter(Donation donation, DonationDTO donationDTO) {
+        donation.setDonor(donationDTO.getIdDonor() != null
+                ? donorService.getById(donationDTO.getIdDonor()) : donation.getDonor());
+        donation.setDescription(donationDTO.getDescription() != null
+                ? donationDTO.getDescription() : donation.getDescription());
+        donation.setQuantity(donationDTO.getQuantity() != null
+                ? donationDTO.getQuantity() : donation.getQuantity());
+
+        return donation;
+    }
 }
--- src/main/java/org/ijb/stock/model/Donor.java
+++ src/main/java/org/ijb/stock/model/Donor.java
@@ -5,8 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_doador")
@@ -21,7 +24,8 @@ public class Donor {
     private String cpf;
     @Column(name = "telefone")
     private String phone;
-
+    @OneToMany(mappedBy = "donor")
+    private Set<Donation> donations;
     public Donor() {
 
     }
