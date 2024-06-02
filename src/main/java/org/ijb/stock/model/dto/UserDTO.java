ff8c3bbfb793eb5d843253ead62d802e2600b717
alyssonlemes <alyssondiamantino@gmail.com>
2024-06-02 16:38:08 -0300

    Add User feature

---
+++ src/main/java/org/ijb/stock/model/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package org.ijb.stock.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+    private String senha;
+}
---
+++ src/main/java/org/ijb/stock/service/UserService.java
@@ -0,0 +1,30 @@
+package org.ijb.stock.service;
+
+import org.ijb.stock.helper.OptionalHelper;
+import org.ijb.stock.model.User;
+import org.ijb.stock.model.dto.UserDTO;
+import org.ijb.stock.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private OptionalHelper optionalHelper;
+
+    private void save(User user){ userRepository.save(user);}
+
+    @Transactional
+    public void createUser(UserDTO userDTO){
+        optionalHelper.isEntityAlreadyExists(userRepository.findByEmail(userDTO.getEmail()));
+
+        User user = new User(userDTO.getEmail(), userDTO.getSenha());
+
+        save(user);
+    }
+
+}
