aec38d9aa9ec095cf6a20f24934727177fd4ece2
alyssonlemes <alyssondiamantino@gmail.com>
2024-06-02 20:10:41 -0300

    add requests user feature

--- src/main/java/org/ijb/stock/service/UserService.java
+++ src/main/java/org/ijb/stock/service/UserService.java
@@ -2,40 +2,77 @@ package org.ijb.stock.service;
 
 import org.ijb.stock.helper.OptionalHelper;
 import org.ijb.stock.helper.UserHelper;
+import org.ijb.stock.model.Donor;
 import org.ijb.stock.model.User;
+import org.ijb.stock.model.dto.DonorDTO;
 import org.ijb.stock.model.dto.UserDTO;
 import org.ijb.stock.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private OptionalHelper optionalHelper;
+
     @Autowired
     private UserHelper userHelper;
 
     private void save(User user){ userRepository.save(user);}
+    private void delete(User user){userRepository.delete(user);}
 
     @Transactional
     public void createUser(UserDTO userDTO){
         optionalHelper.isEntityAlreadyExists(userRepository.findByEmail(userDTO.getEmail()));
 
-        User user = new User(userDTO.getEmail(), userDTO.getSenha());
-
-        save(user);
+        save(userHelper.dtoToUser(userDTO));
     }
 
-    public User findUserById(Integer id) {
+    public UserDTO findUserById(Integer id) {
         User user = optionalHelper.verifyOptionalEntity(userRepository.findById(id));
 
         return userHelper.userToDTO(user);
     }
 
+    public List<UserDTO> findAll() {
+        return userHelper.usersToDTO(userRepository.findAll());
+    }
+
+    @Transactional
+    public void updateUserById(Integer id, UserDTO userDTO) {
+        User user = optionalHelper.verifyOptionalEntity(userRepository.findById(id));
+
+        if(!userDTO.getEmail().equals(user.getEmail())) {
+            optionalHelper.isEntityAlreadyExists(userRepository.findByEmail(userDTO.getEmail()));
+        }
+
+        userRepository.save(userHelper.userUpdateSetter(user, userDTO));
+    }
+
+    @Transactional
+    public void patchUserById(Integer id, UserDTO userDTO) {
+        User user = optionalHelper.verifyOptionalEntity(userRepository.findById(id));
+
+        if(userDTO.getEmail() != null && !userDTO.getEmail().equals(user.getEmail()) ) {
+            optionalHelper.isEntityAlreadyExists(userRepository.findByEmail(userDTO.getEmail()));
+        }
+
+        userRepository.save(userHelper.userPatchUpdateSetter(user, userDTO));
+    }
+
+    @Transactional
+    public void deleteUserById(Integer id) {
+        User user  = optionalHelper.verifyOptionalEntity(userRepository.findById(id));
+
+        delete(user);
+    }
 
 
 }
