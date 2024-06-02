810147a12b14954a9f41882e8938e1292fe4c4c4
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-06-02 18:54:10 -0300

    Add donation crud

--- src/main/java/org/ijb/stock/model/dto/DonationDTO.java
+++ src/main/java/org/ijb/stock/model/dto/DonationDTO.java
@@ -1,2 +1,24 @@
-package org.ijb.stock.model.dto;public class DonationDTO {
+package org.ijb.stock.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class DonationDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Integer id;
+    private Integer idDonor;
+    private String description;
+    private Long quantity;
+
+    public DonationDTO() {
+    }
+
+    public DonationDTO(Integer id, Integer idDonor, String description, Long quantity) {
+        this.id = id;
+        this.description = description;
+        this.idDonor = idDonor;
+        this.quantity = quantity;
+    }
 }
