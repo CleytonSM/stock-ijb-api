3b8c0f266a056d47cb0ef714d5946a8b363c5b2e
alyssonlemes <alyssondiamantino@gmail.com>
2024-06-02 20:10:41 -0300

    add requests user feature

--- src/main/java/org/ijb/stock/model/dto/UserDTO.java
+++ src/main/java/org/ijb/stock/model/dto/UserDTO.java
@@ -1,22 +1,20 @@
 package org.ijb.stock.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class UserDTO {
-
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer id;
     private String email;
-    private String senha;
+    private String password;
 
-    public UserDTO(String email, String senha) {
-        this.email = email;
-        this.senha = senha;
-    }
 
-    public UserDTO(Integer id, String email, String senha) {
+
+    public UserDTO(Integer id, String email, String password) {
         this.id = id;
         this.email = email;
-        this.senha = senha;
+        this.password = password;
     }
 }
