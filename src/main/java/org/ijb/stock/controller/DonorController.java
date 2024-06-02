dba7b5ec1b9a2d5f973f444481ead226ca3fbcf2
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-06-02 15:49:28 -0300

    Add donor MVC

---
+++ src/main/java/org/ijb/stock/controller/DonorController.java
@@ -0,0 +1,27 @@
+package org.ijb.stock.controller;
+
+import org.ijb.stock.model.Donor;
+import org.ijb.stock.model.dto.DonorDTO;
+import org.ijb.stock.service.DonorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/donor")
+public class DonorController {
+
+    @Autowired
+    private DonorService donorService;
+
+    @PostMapping("/register")
+    public ResponseEntity<HttpStatus> registerDonor(@RequestBody DonorDTO donorDTO) {
+        donorService.createDonor(donorDTO);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
