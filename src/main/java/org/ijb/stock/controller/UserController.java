8cb9644b49acd3b2742f28a9389a9778de105b45
alyssonlemes <alyssondiamantino@gmail.com>
2024-06-02 17:16:56 -0300

    add user helper

--- src/main/java/org/ijb/stock/controller/UserController.java
+++ src/main/java/org/ijb/stock/controller/UserController.java
@@ -5,10 +5,9 @@ import org.ijb.stock.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -21,5 +20,15 @@ public class UserController {
     public ResponseEntity<HttpStatus> registerUser(@RequestBody UserDTO userDTO) {
         userService.createUser(userDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/find/id/")
+    public ResponseEntity<UserDTO> findUserById(@RequestParam Integer id){
+        return new ResponseEntity<>(userService.findUserById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/find/all")
+    public ResponseEntity<List<UserDTO>> findAllUsers() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 }
