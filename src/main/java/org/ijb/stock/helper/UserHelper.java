16f0a2ef9d5712b3f2646018aef4a498c782f901
alyssonlemes <alyssondiamantino@gmail.com>
2024-06-02 20:10:41 -0300

    add requests user feature

--- src/main/java/org/ijb/stock/helper/UserHelper.java
+++ src/main/java/org/ijb/stock/helper/UserHelper.java
@@ -2,9 +2,45 @@ package org.ijb.stock.helper;
 
 import org.ijb.stock.model.User;
 import org.ijb.stock.model.dto.UserDTO;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class UserHelper {
     public UserDTO userToDTO(User user) {
-        return new UserDTO(user.getEmail(), user.getSenha());
+        return new UserDTO(user.getId(), user.getEmail(), user.getPassword());
     }
+
+
+    public List<UserDTO> usersToDTO(List<User> users) {
+        List<UserDTO> userDTOS = new ArrayList<>();
+
+        for (User user : users) {
+            userDTOS.add(userToDTO(user));
+        }
+
+        return userDTOS;
+    }
+
+    public User dtoToUser(UserDTO userDTO) {
+        return new User(userDTO.getEmail(), userDTO.getPassword());
+    }
+
+    public User userUpdateSetter(User user, UserDTO userDTO) {
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+
+        return user;
+    }
+
+    public User userPatchUpdateSetter(User user, UserDTO userDTO) {
+        user.setEmail(userDTO.getEmail() != null ? userDTO.getEmail() : user.getEmail());
+        user.setPassword(userDTO.getPassword() != null ? userDTO.getPassword() : user.getPassword());
+
+        return user;
+    }
+
+
 }
